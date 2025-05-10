9fe94adb51c9aec83d4123b8b56370526c507ef5
José Vinícius <jvprotano@gmail.com>
2025-05-10 13:10:14 -0300

    Update ApiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.vibe-sync.com/api',
+    apiUrl: 'https://vibesync-czb4fjedcycre0bb.eastus-01.azurewebsites.net/api',
     baseUrl: 'http://localhost:4200'
 };
